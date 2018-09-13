e9bb777f0e0c020935dee9fe023986b1c9d5a942
rvpanoz <rvpanoz@gmail.com>
2018-09-13 23:45:31 +0300

    fix actions and types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,91 +1,34 @@
 import config from "../config";
 import { combineEpics, ofType } from "redux-observable";
+import { mapTo, mergeMap, filter, catchError } from "rxjs/operators";
 import {
-  mapTo,
-  mergeMap,
-  filter,
-  catchError,
-  switchMap,
-  finalize
-} from "rxjs/operators";
-
-///
-const filters = [
-  {
-    name: "limit",
-    value: 10
-  },
-  {
-    name: "genres",
-    value: "deep house"
-  }
-];
-
-const criteria = filters.reduce((acc, filterDet, idx) => {
-  const joinValues = obj => Object.values(obj).join("=");
-  return [...acc, joinValues(filterDet)];
-}, []);
-
-const URL = `${config.searchUrl}?client_id=${config.client_id}&${criteria.join(
-  "&"
-)}`;
-///
-
-import {
-  ADD_TRACKS,
-  ADD_FILTER,
-  FETCH_TRACKS_LOADING,
-  FETCH_TRACKS_ERROR,
-  FETCH_TRACKS_SUCCESS
+  ADD_TODO,
+  FETCH_TODOS_LOADING,
+  FETCH_TODOS_ERROR,
+  FETCH_TODOS_SUCCESS
 } from "./types";
 
-export const addTracks = tracks => ({
-  type: ADD_TRACKS,
+const fetchTodosSuccess = action => ({
+  type: FETCH_TODOS_SUCCESS,
   loading: false,
   tracks
 });
 
-export const fetchTracksSuccess = tracks => ({
-  type: FETCH_TRACKS_SUCCESS,
-  loading: false,
-  tracks
-});
-
-export const fetchTracksError = error => ({
-  type: FETCH_TRACKS_ERROR,
-  loading: false,
-  tracks: [],
-  error
-});
-
-export const fetchTracksStart = () => ({
-  type: FETCH_TRACKS_LOADING,
-  loading: true
-});
-
-export function addFilter(name, value) {
-  return {
-    type: ADD_FILTER,
-    name,
-    value
-  };
-}
-
-const fetchTracksEpic = (action$, state$) =>
+const fetchTodoEpic = (action$, state$) =>
   action$.pipe(
-    ofType(FETCH_TRACKS_LOADING),
+    ofType(FETCH_TODOS_LOADING),
     catchError(error =>
       of({
-        type: FETCH_TRACKS_ERROR,
+        type: FETCH_TODOS_ERROR,
         payload: error.xhr.response,
         error: true
       })
     ),
     mergeMap(async () => {
-      const response = await fetch(URL);
-      const tracks = await response.json();
-      return fetchTracksSuccess(tracks);
+      const response = await fetch(config.URL);
+      const todos = await response.json();
+      return fetchTracksSuccess(todos);
     })
   );
 
-export const rootEpic = combineEpics(fetchTracksEpic);
+export const rootEpic = combineEpics(fetchTodosEpic);
