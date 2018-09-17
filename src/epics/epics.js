521e7fb8180d6d6fcc7baf0660900ba283ed4c79
dlivas <dimitris.livas@gmail.com>
2018-09-17 15:43:37 +0100

    remove some unused imports

--- src/epics/epics.js
+++ src/epics/epics.js
@@ -1,9 +1,7 @@
 import { Observable, throwError, of } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
-import { combineEpics } from "redux-observable";
-
-import { map, mapTo, switchMap, catchError } from "rxjs/operators";
+import { map, switchMap, catchError } from "rxjs/operators";
 
 import config from "../config";
 
@@ -13,13 +11,11 @@ import {
   fetchTodosSuccess,
 } from "../actions";
 
-const fetchTodosEpic = (action$, state) =>
+const fetchTodosEpic = (action$, state$) =>
   action$.ofType(fetchTodosStart.type).pipe(
     switchMap(() => ajax.getJSON(config.todosUrl)),
     map(({ data: todos}) => fetchTodosSuccess({ todos })),
     catchError(error => fetchTodosError({ error })),
   );
-
-// export const rootEpic = combineEpics(fetchTodosEpic);
 
 export default fetchTodosEpic;
