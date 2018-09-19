252a0b092d70e08b9b3e39d50697ecb0e6dd0b9f
rvpanoz <rvpanoz@gmail.com>
2018-09-19 23:26:31 +0300

    use Ramda in todos reducer

--- src/models/todos/reducers.js
+++ src/models/todos/reducers.js
@@ -1,4 +1,4 @@
-import { identity, assoc, propOr, prop, merge } from "ramda";
+import { identity, assoc, propOr, prepend, merge } from "ramda";
 
 import {
   addTodo,
@@ -16,28 +16,22 @@ const initialState = {
 };
 
 const handlers = {
-  [addTodo.type]: (state, { payload: { todo } }) => ({
-    ...state,
-    todos: [...state.todos, todo]
-  }),
-  [fetchTodosStart.type]: state => ({
-    ...state,
-    loading: true,
-    todos: [...state.todos]
-  }),
-  [fetchTodosSuccess.type]: (state, { payload: { todos } }) => ({
-    ...state,
-    loading: false,
-    todos: [...state.todos, ...todos]
-  }),
-  [fetchTodosError.type]: (state, { payload: { error } }) => ({
-    ...state,
-    error
-  }),
-  [todosFiltered.type]: (state, { payload: { todos } }) => ({
-    ...state,
-    todos: [...state.todos, ...todos]
-  })
+  [addTodo.type]: (state, { payload: { todo } }) =>
+    assoc("todos", prepend(todo, state.todos), state),
+  [fetchTodosStart.type]: state =>
+    merge(state, {
+      loading: true,
+      todos: state.todos
+    }),
+  [fetchTodosSuccess.type]: (state, { payload: { todos } }) =>
+    merge(state, {
+      loading: false,
+      todos
+    }),
+  [fetchTodosError.type]: (state, { payload: { error } }) =>
+    assoc("error", error, state),
+  [todosFiltered.type]: (state, { payload: { todos } }) =>
+    assoc("todos", todos, state)
 };
 
 const reducer = (state = initialState, action) =>
