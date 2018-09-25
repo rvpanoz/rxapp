2d6281a175d4113b53f3336edb9e518efe1ba07b
rvpanoz <rvpanoz@gmail.com>
2018-09-26 01:12:36 +0300

    add more todos (mock)

--- mock.js
+++ mock.js
@@ -17,6 +17,42 @@ export function getTodos() {
       title: "Silly todo 3",
       completed: 0,
       created_at: new Date()
+    },
+    {
+      id: 112,
+      title: "Silly todo 4",
+      completed: 0,
+      created_at: new Date()
+    },
+    {
+      id: 34,
+      title: "Silly todo 5",
+      completed: 1,
+      created_at: new Date()
+    },
+    {
+      id: 599,
+      title: "Silly todo 6",
+      completed: 0,
+      created_at: new Date()
+    },
+    {
+      id: 346,
+      title: "Silly todo 7",
+      completed: 0,
+      created_at: new Date()
+    },
+    {
+      id: 178,
+      title: "Silly todo 8",
+      completed: 1,
+      created_at: new Date()
+    },
+    {
+      id: 199,
+      title: "Silly todo 9",
+      completed: 0,
+      created_at: new Date()
     }
   ];
 }
