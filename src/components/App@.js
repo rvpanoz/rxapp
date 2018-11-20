eadaa373530e86c1d420cc4389d752235514e24d
v-parvanitis <v-parvanitis@eurobank.gr>
2018-11-20 14:09:53 +0200

    WIP# add redux-react-hook - trying hooks with redux

--- src/components/App@.js
+++ src/components/App@.js
@@ -1,11 +1,7 @@
 import React from "react";
-
 import { connect } from "react-redux";
-
 import mapProps from "commons/mapProps";
-
 import { loading } from "models/todos/selectors";
-
 import App from "./App";
 
 export default connect(mapProps({ loading }))(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { Provider } from "react-redux";
-
 import App from "components";
-
 import configureStore from "store/configureStore";
 
 const store = configureStore();
